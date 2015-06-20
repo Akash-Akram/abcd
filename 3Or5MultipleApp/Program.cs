03365afa59884eb2ec52c93e8bcf4bd2bbca2b41
Akash-Akram <mdakashakram@gmail.com>
2015-06-20 12:39:46 +0600

    Upload project 1st step

---
+++ 3Or5MultipleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3Or5MultipleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum=0;
+            for (int i = 0; i < 1000; i++)
+            {
+                if (i%5 == 0 || i%5 == 0)
+                {
+                     sum  += i;
+                }
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
